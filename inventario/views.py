cb43c5a8ea892f1dea3ded468bc085a8329d342e
IgnacioBiskupsky1 <ig.biskupsky@duocuc.cl>
2024-05-20 21:01:03 -0400

    se ordeno carpetas insumos y mp, se agregaron ordenar por atributos  y se refino el base.html

--- inventario/views.py
+++ inventario/views.py
@@ -60,9 +60,35 @@ def edituser(request):
     return render(request, 'usercrud/edituser.html', context) #BORRAR ESTA COSA
 
 
+
+
+
+
+
+
+
+
+
+# VIEWS MATERIAS PRIMAS
+
 def crud_mp(request):
+    # Obtener parámetros de filtrado y ordenado de la URL
+    ordenar_por = request.GET.get('ordenar_por', 'adtv_id,adtv_nom,adtv_dens')
+
     mps = InfoAditivo.objects.all()
-    return render(request, 'inventary/crud_mp.html', {'mps': mps})
+
+    # Ordenar MP
+    ordenar_por_campos = ordenar_por.split(',')
+    mps = mps.order_by(*ordenar_por_campos)
+
+    
+    return render(request, 'inventary/materia_prima/crud_mp.html', {'mps': mps})
+
+
+
+#def crud_mp(request):
+#    mps = InfoAditivo.objects.all()
+#    return render(request, 'inventary/materia_prima/crud_mp.html', {'mps': mps})
 
 def ingresar_mp(request):
     if request.method == 'POST':
@@ -72,7 +98,7 @@ def ingresar_mp(request):
             return redirect('/crud_mp')  # Cambia 'home' por el nombre de tu URL de inicio
     else:
         form = MpForm()
-    return render(request, 'inventary/ingresar_mp.html', {'form': form})
+    return render(request, 'inventary/materia_prima/ingresar_mp.html', {'form': form})
 
 def editar_mp(request, adtv_id):
     aditivo = get_object_or_404(InfoAditivo, adtv_id=adtv_id)
@@ -83,28 +109,61 @@ def editar_mp(request, adtv_id):
             return redirect('/crud_mp')  # Cambia 'listar_aditivos' por el nombre de tu vista de listado de aditivos
     else:
         form = InfoAditivoForm(instance=aditivo)
-    return render(request, 'inventary/editar_mp.html', {'form': form})
+    return render(request, 'inventary/materia_prima/editar_mp.html', {'form': form})
 
 def eliminar_mp(request, adtv_id):
     aditivo = get_object_or_404(InfoAditivo, adtv_id=adtv_id)
     if request.method == 'POST':
         aditivo.delete()
         return redirect('/crud_mp')  # Redirigir a la página de listado de productos
-    return render(request, 'inventary/eliminar_mp.html', {'aditivo': aditivo})
+    return render(request, 'inventary/materia_prima/eliminar_mp.html', {'aditivo': aditivo})
 
 def listar_mp(request):
     mps = InfoAditivo.objects.all()
-    return render(request, 'inventary/listar_mp.html', {'mps': mps}) 
+    return render(request, 'inventary/materia_prima/listar_mp.html', {'mps': mps}) 
+
+
+
+
+
+
+
+
+
+# VIEWS INSUMOS
 
 
 
 def crud_insu(request):
+    # Obtener parámetros de filtrado y ordenado de la URL
+    ordenar_por = request.GET.get('ordenar_por', 'in_nombre,insumo_id')
+#   filtrar_cantidad = request.GET.get('filtrar_cantidad', None)
+
+    # Obtener todos los insumos
     insus = Insumo.objects.all()
-    return render(request, 'inventary/crud_insu.html', {'insus': insus}) 
+
+    # Filtrar insumos si es necesario
+#    if filtrar_cantidad is not None:
+#        insus = insus.filter(cantidad__gt=0)
+
+    # Ordenar insumos
+    ordenar_por_campos = ordenar_por.split(',')
+    insus = insus.order_by(*ordenar_por_campos)
+
+    
+    return render(request, 'inventary/insumos/crud_insu.html', {'insus': insus})
+
+
+#def crud_insu(request):
+#    insus = Insumo.objects.all()
+#    return render(request, 'inventary/crud_insu.html', {'insus': insus}) 
+
+
+
 
 def listar_in(request):
     insus = Insumo.objects.all()
-    return render(request, 'inventary/listar_insumo.html', {'insus': insus})
+    return render(request, 'inventary/insumos/listar_insumo.html', {'insus': insus})
 
 def ingresar_in(request):
     if request.method == 'POST':
@@ -114,14 +173,14 @@ def ingresar_in(request):
             return redirect('/crud_insu')  # Cambia 'home' por el nombre de tu URL de inicio
     else:
         form = InForm()
-    return render(request, 'inventary/ingresar_insumo.html', {'form': form})
+    return render(request, 'inventary/insumos/ingresar_insumo.html', {'form': form})
 
 def eliminar_in(request, insumo_id):
     insumo = get_object_or_404(Insumo, insumo_id=insumo_id)
     if request.method == 'POST':
         insumo.delete()
         return redirect('/crud_insu')  # Redirigir a la página de listado de productos
-    return render(request, 'inventary/eliminar_insumo.html', {'insumo': insumo})
+    return render(request, 'inventary/insumos/eliminar_insumo.html', {'insumo': insumo})
 
 def editar_in(request, insumo_id):
     insumo = get_object_or_404(Insumo, insumo_id=insumo_id)
@@ -132,4 +191,4 @@ def editar_in(request, insumo_id):
             return redirect('/crud_insu')  
     else:
         form = InForm(instance=insumo)
-    return render(request, 'inventary/editar_insumo.html', {'form': form})
+    return render(request, 'inventary/insumos/editar_insumo.html', {'form': form})
